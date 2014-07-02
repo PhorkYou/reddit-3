4f926bdb0606e4f1741de3bc22dbdf4ee8f277fe
cmathew.weber@gmail.com <weberc@AACCOFF5832.fortisclearingus.com>
2014-07-02 15:58:52 -0500

    reddit_nonwebapp.py

--- reddit_nonwebapp.py
+++ reddit_nonwebapp.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 import requests
-import requests.auth
 import time
 from pprint import pprint
+import json
+import os
 
 CLIENT_ID = ""
 CLIENT_SECRET = ""
 
+#def key_in_file():
+#	with open('key.txt') as f:
+#		if (time.ctime(os.path.getmtime(f)) > 3600:
+#			get_a_new_key()
+#		else:
+#			use_existing()
+			
 def login():
 	#API keys that authorize the script
 	auth_script = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
@@ -23,12 +31,19 @@ def login():
 	while login_attempt < 5:
 		response = requests.post(access_url, data=login_params, auth=auth_script)
 		rc = int(response.status_code)
-		if rc == 429:
+		if 
+
+		elif rc == 429:
 			login_attempt += 1
 			print "Attempting to login again"
 			time.sleep(2)
 		elif rc == 200:
 			print rc
+			with open('key.txt', 'w+') as f:
+				resp_list = response.json()
+				at = resp_list['access_token']
+				f.write(at)
+				f.close()
 			break
 		elif login_attempt == 5:
 			print "Could not get a token with alloted number of attempts. Script will exit."
@@ -38,13 +53,15 @@ def login():
 			print rc
 			break
 			exit()
-	resp_list = response.json()
-	at = resp_list['access_token']
 	return at
 	
 
 if __name__ == "__main__":
-	headers = {"Authorization": "bearer %s" % login()}
-	response = requests.get("https://oauth.reddit.com", headers=headers)
-	print pprint(response.text)
+	headers = {
+			"Authorization": "bearer %s" % login(),
+			"User-Agent": "stoicaureliusscript/1.0 by stoicaurelius"}
+	access_url = "https://oauth.reddit.com/api/v1/me/karma"
+	response = requests.get(access_url, headers=headers)
+	data = json.loads(response.text)
+	print str(data['data'][0]['comment_karma']) + ' ' + str(data['data'][0]['sr'])
 
